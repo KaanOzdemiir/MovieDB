479814bce0e007469d50877c13a7c03bab44f389
Kaan Ozdemir <kaan.ozdemir@tsoft.com.tr>
2020-10-21 17:56:10 +0300

    .

--- MovieDB/Helper/Extension.swift
+++ MovieDB/Helper/Extension.swift
@@ -69,3 +69,16 @@ extension UIView {
         self.layer.insertSublayer(gradient, at: 0)
     }
 }
+
+extension CustomStringConvertible{
+    var description: String{
+        var desc = "Class Name: \(type(of: self))\n"
+        let mirror = Mirror(reflecting: self)
+        for child in mirror.children {
+            if let label = child.label {
+                desc.append("\(label): \(child.value)\n")
+            }
+        }
+        return desc
+    }
+}
